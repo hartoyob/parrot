d2bf592f2e9de8c210670e4337fd1ba9ee7ce0d5
suntzu <suntzu@suntzus-MBP.lan>
2021-04-07 08:25:25 +0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker run --rm -d -v squid:/data --name squid-deb-proxy squid-deb-proxy
-docker build  \
+docker build --no-cache \
 --build-arg http_proxy=http://$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' squid-deb-proxy):8000 . -t suntzu/parrot
 docker stop squid-deb-proxy 
